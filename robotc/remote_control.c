4a43e6aa0bd6dd5b452ad9d314771ae665c5c616
Nick <nickswaerdens@gmail.com>
2017-04-06 14:47:29 +0200

    Update remote_control.c

--- robotc/remote_control.c
+++ robotc/remote_control.c
@@ -1,34 +1,41 @@
 void turn_left()
 {
-  motor[motorB] = 30;
-  motor[motorC] = 0;
-  wait1Msec(50);
+	motor[motorB] = 30;
+	motor[motorC] = 0;
+	wait1Msec(50);
 }
 
 void turn_right()
 {
-  motor[motorB] = 0;
-  motor[motorC] = 30;
-  wait1Msec(50);
+	motor[motorB] = 0;
+	motor[motorC] = 30;
+	wait1Msec(50);
 }
 
 void move_forward()
 {
 	motor[motorB] = 30;
 	motor[motorC] = 30;
-  wait1Msec(50);
+	wait1Msec(50);
 }
 
-void turn_backward()
+void turn_backwards()
 {
-  motor[motorB] = 30;
-  motor[motorC] = -30;
-  wait1Msec(50);
+	motor[motorB] = 30;
+	motor[motorC] = -30;
+	wait1Msec(50);
 }
 
 void stop_motor()
+{ 
+	motor[motorB] = 0;
+	motor[motorC] = 0;
+	wait1Msec(50);
+}
+
+void shoot()
 {
-  motor[motorB] = 0;
-  motor[motorC] = 0;
-  wait1Msec(50);
+	motor(motorA) = 2;
+	wait1Msec(3000);
+	motor(motorA) = 0;
 }
